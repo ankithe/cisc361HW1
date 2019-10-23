113d86f46ca6eb072446a7910cf5743e692edae4
Ankith Ede <ankithe@gmail.com>
2019-10-23 00:08:35 -0400

    leak fix again

--- myshell/sh.c
+++ myshell/sh.c
@@ -364,8 +364,6 @@ int sh(int argc, char **argv, char **envp)
               else if(pid != 0){
                 waitpid(pid,NULL,0);
               }
-              free(cmd_path);
-
           }
           else{
                 printf("Command not found: %s\n",args[0]);
